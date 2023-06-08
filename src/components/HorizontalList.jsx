680298e58fe6637d0e245c67cd9fa2b32422e293
KoderGoq <FernandoBussines10@gmail.com>
2023-06-08 17:30:48 -0600

    init components
    Co-Authored-By: Montserrat Aguilar <116055107+montsegv-2@users.noreply.github.com>
    Co-Authored-By: Arnoldo Magaña <91897457+AMCernas@users.noreply.github.com>
    Co-Authored-By: AlbertoG22 <47892591+AlbertoG22@users.noreply.github.com>

---
+++ src/components/HorizontalList.jsx
@@ -0,0 +1,8 @@
+
+const HorizontalList = () => {
+    return (
+        <h1>Lita Horizontal</h1>
+    )
+}
+
+export default HorizontalList
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,8 @@
+
+const SearchBar = () => {
+    return (
+        <h1>Desde el buscador</h1>
+    )
+}
+
+export default SearchBar
