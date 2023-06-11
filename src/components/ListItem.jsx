b7cd9be2bb269324843232c538075fa7cd055d00
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2023-06-10 20:15:15 -0600

    Se añadio el componente Modal y TouchableOpacity

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,12 +1,20 @@
 import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import ModalDetails from './ModalDetails';
+import { useState } from 'react';
 
 const ListItem = ({ dish }) => {
-    return (
-        <TouchableOpacity style={styles.itemContainer}>
-          <Image source={{uri: dish.image}} style={styles.itemImage}  />
-          <Text style={styles.itemText}>{dish.name}</Text>
-        </TouchableOpacity>
-    );
+
+  const [isModalDetailOpen, setIsModalDetailOpen] = useState(false);
+
+  console.log(isModalDetailOpen);
+
+  return (
+      <TouchableOpacity style={styles.itemContainer} onPress={() => setIsModalDetailOpen(true)}>
+        <ModalDetails visible={isModalDetailOpen} />
+        <Image source={{uri: dish.image}} style={styles.itemImage}  />
+        <Text style={styles.itemText}>{dish.name}</Text>
+      </TouchableOpacity>
+  );
 }
 
 const styles = StyleSheet.create({
--- src/components/ModalDetails.jsx
+++ src/components/ModalDetails.jsx
@@ -1,34 +1,36 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, Image, TouchableOpacity, FlatList, StyleSheet, ScrollView } from "react-native";
+import { View, Text, Image, TouchableOpacity, FlatList, StyleSheet, ScrollView, Modal } from 'react-native';
 import data from '../data/aliments.json';
 
-const ModalDetails = () => {
+const ModalDetails = ({ visible }) => {
 
     const [receipData, setReceipData] = useState(data[0]);
 
     return (
-        <View>
-            <Image
-                style={styles.image}
-                source={{
-                    uri: receipData.image,
-                }}
-            />
-            <Text>{receipData.category}</Text>
-            <Text>{receipData.name}</Text>
-            <Text>{`Ingredients\nfor ${receipData.servings} servings`}</Text>
-            <FlatList 
-                data={receipData.ingredients}
-                renderItem={({ item }) => (
-                    <ScrollView>
-                        <View>
-                            <Text>{item.name}</Text>
-                            <Text>{`${item.quantity} ${item.unit}`}</Text>
-                        </View>
-                    </ScrollView>
-                )}
-            />
-        </View>
+        <Modal visible={ visible } animationType='slide'>
+            <View>
+                <Image
+                    style={styles.image}
+                    source={{
+                        uri: receipData.image,
+                    }}
+                />
+                <Text>{receipData.category}</Text>
+                <Text>{receipData.name}</Text>
+                <Text>{`Ingredients\nfor ${receipData.servings} servings`}</Text>
+                <FlatList 
+                    data={receipData.ingredients}
+                    renderItem={({ item }) => (
+                        <ScrollView>
+                            <View>
+                                <Text>{item.name}</Text>
+                                <Text>{`${item.quantity} ${item.unit}`}</Text>
+                            </View>
+                        </ScrollView>
+                    )}
+                />
+            </View>
+        </Modal>
     );
 }
 
